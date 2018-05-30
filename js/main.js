252584f4d20fae67851458048747bcad6e88571a
Vlad <kosachevlad@gmail.com>
2018-05-30 13:43:26 +0300

    Это наш третий коммит

--- js/main.js
+++ js/main.js
@@ -1,31 +1,20 @@
-// function printTime(){
-// 	var d = new Date();
-// 	var hours = d.getHours();
-// 	var mins = d.getMinutes();
-// 	var secs = d.getSeconds();
-// 	document.body.innerHTML = hours+" : "+mins+" : "+secs;
-// }
-// setInterval(printTime, 1000)
+let monthBudget,
+	shopName,
+	mainList = {
+		budget: 100,
+		shopName: '',
+		shopGoods: [],
+		employers: {},
+		open: true
+	}
 
 
-// var answer = +prompt("Вам есть 18?", "18")
 
-// console.log( typeof(answer));
+let shopGoods = Array(
+		prompt("Какой тип товаров будем продавать?"), 
+		prompt("Какой еще тип товаров будем продавать?"), 
+		prompt("Какой еще тип товаров будем продавать?")
+	);
 
-var incr = 10;
-	decr = 10;
+alert(monthBudget / 30);
 
-// incr++;
-// decr--;
-
-console.log(incr);
-console.log(decr);
-
-console.log(5%2);
-
-console.log(incr == 10);
-
-var isChecked = true;
-var isClosed = true;
-
-console.log(isChecked || !isClosed);
