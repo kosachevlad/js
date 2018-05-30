3fd5312c89f56273944b881e2880990caca2ebbb
Vlad <kosachevlad@gmail.com>
2018-05-30 12:06:02 +0300

    Это наш первый коммит

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// function printTime(){
+// 	var d = new Date();
+// 	var hours = d.getHours();
+// 	var mins = d.getMinutes();
+// 	var secs = d.getSeconds();
+// 	document.body.innerHTML = hours+" : "+mins+" : "+secs;
+// }
+// setInterval(printTime, 1000)
+
+
+// var answer = +prompt("Вам есть 18?", "18")
+
+// console.log( typeof(answer));
+
+var incr = 10;
+	decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+
+console.log(incr == 10);
+
+var isChecked = true;
+var isClosed = true;
+
+console.log(isChecked || !isClosed);
