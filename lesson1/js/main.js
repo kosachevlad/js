7fbd3c9868b8e5063cc4c3319e7476a8b24b47c3
Vlad <kosachevlad@gmail.com>
2018-06-03 19:44:26 +0300

    Это наш третий коммит

--- lesson1/js/main.js
+++ lesson1/js/main.js
@@ -1,74 +1,88 @@
-let monthBudget = prompt("Ваш бюджет на месяц?"),
-	shopName = prompt("Название вашего магазина");
-let time = 19;
+let monthBudget,
+	shopName,
+	time,
+	price
+
+function start() {
+	monthBudget = prompt("Ваш бюджет");
+
+	while (isNaN(monthBudget) || monthBudget == "" || monthBudget == null) {
+		monthBudget = prompt("Ваш бюджет");
+	}
+
+	shopName = prompt("Название вашего магазина").toUpperCase();
+	time = 21;
+
+}
+start();
 
 let	mainList = {
 		budget: monthBudget,
 		shopName: shopName,
 		shopGoods: [],
 		employers: {},
-		open: true
+		open: true,
+		discount: true
 	}
+function chooseGoods() {
+	for (let i = 0; i < 5; i++) {
 
-for (let i = 0; i < 5; i++) {
+		let a = prompt("Какой тип товаров будем продавать?");
+		
+		if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50 ) {
+			console.log('Верно');
+			mainList.shopGoods[i] = a;
+		} else {
+			console.log('НеВерно');
+			alert('Были введены неверные значения! Попробуйте еще раз!');
+			i--
+		}
+	}
+}
+chooseGoods();
 
-	let a = prompt("Какой тип товаров будем продавать?");
-	
-	if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50 ) {
-		console.log('Верно');
-		mainList.shopGoods[i] = a;
+function workTime(time) {
+	if (time < 0) {
+		console.log('Такого не может быть!');
+	} else if(time > 8 && time < 20) {
+		console.log('Время работать!')
+		} else if(time < 24) {
+			console.log('Уже слишком поздно!');
+		} else {
+			console.log('В сутках только 24 часа!');
+		}
+}
+workTime(18)
+
+function dayBudget() {
+	alert('Бюджет на 1 день = ' + monthBudget / 30);
+}
+dayBudget();
+
+function discountSystem(discount) {
+	if (mainList.discount == true) {
+		price = '80%';
 	} else {
-		console.log('НеВерно');
-		alert('Были введены неверные значения! Попробуйте еще раз!');
-		i--
+		price
 	}
 }
 
-let i = 0; 
-// Цикл while
-// while (i < 5) {
-	
-// 	let a = prompt("Какой тип товаров будем продавать?");
-	
-// 	if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50 ) {
-// 		console.log('Верно');
-// 		mainList.shopGoods[i] = a;
-// 	} else {
-// 		console.log('НеВерно');
-// 		alert('Были введены неверные значения! Попробуйте еще раз!');
-// 		i--
-// 	}
-// 	i++
-// }
+function employers() {
+	for (let i = 0; i < 4; i++) {
 
-// Цикл do
-// do {
-
-// 	let a = prompt("Какой тип товаров будем продавать?");
-	
-// 	if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50 ) {
-// 		console.log('Верно');
-// 		mainList.shopGoods[i] = a;
-// 	} else {
-// 		console.log('НеВерно');
-// 		alert('Были введены неверные значения! Попробуйте еще раз!');
-// 		i--
-// 	}
-// 	i++
-// } 
-// while (i < 5);
-
-
-if (time < 0) {
-	console.log('impossible');
-} else if(time > 8 && time < 20) {
-	console.log('Time to work')
-	} else if(time < 24) {
-		console.log('Sorry! Too late');
-	} else {
-		console.log('sorry only 24hours');
+		let a = prompt("Как зовут вашего сотрудника");
+		
+		if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50 ) {
+			console.log('Верно');
+			mainList.employers[i] = a;
+		} else {
+			console.log('НеВерно');
+			alert('Были введены неверные значения! Попробуйте еще раз!');
+			i--
+		}
 	}
+}
 
-alert('Бюджет на 1 день = ' + monthBudget / 30);
+console.log(price)
 console.log(mainList);
 
---
+++ lesson3/js/main.js
@@ -0,0 +1,5 @@
+let str = "12.2px";
+
+console.log(parseInt(str));
+console.log(parseFloat(str));
+
