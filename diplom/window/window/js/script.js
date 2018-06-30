9e9e54cecb7899c91809e91ba26001ef2eadd2b4
Vlad <kosachevlad@gmail.com>
2018-06-30 19:50:21 +0300

    диплом 1й коммит

---
+++ diplom/window/window/js/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', () => {
+	let headerBtn = document.getElementsByClassName('header_btn')[0],
+		popupEngineer = document.getElementsByClassName('popup_engineer')[0],
+		close = document.getElementsByClassName('popup_close')[1];
+
+	headerBtn.addEventListener('click', () => {
+		popupEngineer.style.display = 'block';
+	});
+	close.addEventListener('click', function() {
+		popupEngineer.style.display = 'none';
+	});
+});
