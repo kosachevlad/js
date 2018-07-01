d4a7f7d7c9eb0f487161a0ff4a5c84923e093a19
Vlad <kosachevlad@gmail.com>
2018-07-01 13:05:58 +0300

    диплом 2й коммит

--- diplom/window/window/js/script.js
+++ diplom/window/window/js/script.js
@@ -1,17 +1,20 @@
 window.addEventListener('DOMContentLoaded', () => {
-	let headerBtn = document.getElementsByClassName('header_btn')[0],
-		popupEngineer = document.getElementsByClassName('popup_engineer')[0],
-		close = document.getElementsByClassName('popup_close')[1];
+	function modal(){
+		let headerBtn = document.getElementsByClassName('header_btn')[0],
+			popupEngineer = document.getElementsByClassName('popup_engineer')[0],
+			close = document.getElementsByClassName('popup_close')[1];
 
-	headerBtn.addEventListener('click', () => {
-		popupEngineer.style.display = 'block';
-	});
-	close.addEventListener('click', function() {
-		popupEngineer.style.display = 'none';
-	});
-	popupEngineer.addEventListener('click', function(){
-		if(event.target == this) {
+		headerBtn.addEventListener('click', () => {
+			popupEngineer.style.display = 'block';
+		});
+		close.addEventListener('click', () => {
 			popupEngineer.style.display = 'none';
-		}
-	});
+		});
+		popupEngineer.addEventListener('click', function(){
+			if(event.target == this) {
+				popupEngineer.style.display = 'none';
+			}
+		});
+	};
+	modal();
 });
