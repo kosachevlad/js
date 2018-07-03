00779063461b88ab807872fe2f8e0a223ba782ed
Vlad <kosachevlad@gmail.com>
2018-07-03 13:29:32 +0300

    Диплом 7й коммит

--- diplom/window/window/js/script.js
+++ diplom/window/window/js/script.js
@@ -125,6 +125,24 @@ window.addEventListener('DOMContentLoaded', () => {
 			});
 		};
 		check(phoneInput);
+	};
+	// Табы
+
+	let tab = document.getElementsByClassName('glazing_block'),
+		tabContent = document.getElementsByClassName('glazing_tab');
+
+	for (let i = 0; i < tab.length; i++) {
+		tab[i].addEventListener('click', function(){
+			for(let j = 0; j < tab.length; j++) {
+				if (j == i) {
+					tab[j].classList.add('active');
+					tabContent[j].style.display = 'block';
+				} else {
+					tab[j].classList.remove('active');
+					tabContent[j].style.display = 'none';
+				}
+			}
+		})
 	}
 
 });
